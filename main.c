30993e2be313855cc07c403460c3d82eecd65811
Simone Romanello <rommyalvise@hotmail.it>
2019-05-23 13:07:44 +0200

    wip

--- main.c
+++ main.c
@@ -6,17 +6,10 @@
  * Come passare nome file?
  */
 
-/*int main() {
+int main() {
 
     int ciao = 0;
     printf("Hello World!\n");
     ciao = printInstructions("factorial.txt");
-    return 0;
-}*/
-
-int main(void) {
-    char buf[255];
-    getcwd(buf, sizeof(buf));
-    printf("Current working directory is %s\n", buf);
     return 0;
 }
--- printInstructions.c
+++ printInstructions.c
@@ -3,9 +3,9 @@
 #include "CVM.h"
 
 int printInstructions(const char *input){
-    int n_lines, first_read, size = 0;
+    int n_lines, first_read;
     FILE *in;
-    char* line_buf_in = NULL, ciao, temp_char;
+    char *line_buf_in = NULL, *ciao, temp_char[60];
     size_t line_in_buf_size = 0;
 
     in = fopen(input, "r");
@@ -16,16 +16,16 @@ int printInstructions(const char *input){
          * Il seguente segmento di codice mi permette di saltare tutte
          * le linee che non contengono un numero.
          */
-        temp_char = *fgets(&temp_char, strlen(&temp_char), in);
-        while ((temp_char < 47) || (temp_char > 57)){
-            printf("%s", &temp_char);
+        while (fgets(temp_char, 60, in)){
+            ciao = strtok(temp_char, ";");
+            if((*ciao > 47) && (*ciao < 58)){
+                printf("%s\n", ciao);
+            }
         }
+
 
         //Problema: se il numero di linee è di due o più cifre, come lo vedo come int?
 
-        while(getline(&line_buf_in, &line_in_buf_size, in)){
-
-        }
 
     } else {
         printf("Errore nell'apertura del file\n");
